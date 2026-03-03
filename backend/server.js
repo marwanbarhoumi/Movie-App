d52f8266423aeb1ea79608ddbf85c73aa9528cf9
marwan2 <marwanbarhou77@gmail.oom.com>
2026-03-03 15:20:14 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // ✅ مهم للـ preflight
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 /* =========================
    Middlewares
